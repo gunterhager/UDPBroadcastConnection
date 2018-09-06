461c723bc7b1ccf99fc1bb112a3979e79e79b13d
Daniel K. Poulsen <daniel.poulsen@cambridgeaudio.com>
2018-09-06 16:51:07 +0100

    added support for tvOS, macOS, watchOS

---
+++ UDPBroadcast tvOS/UDPBroadcast_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  UDPBroadcast_tvOS.h
+//  UDPBroadcast tvOS
+//
+//  Created by Daniel K. Poulsen on 06/09/2018.
+//  Copyright © 2018 Gunter Hager. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UDPBroadcast_tvOS.
+FOUNDATION_EXPORT double UDPBroadcast_tvOSVersionNumber;
+
+//! Project version string for UDPBroadcast_tvOS.
+FOUNDATION_EXPORT const unsigned char UDPBroadcast_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UDPBroadcast_tvOS/PublicHeader.h>
+
+
